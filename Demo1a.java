19fb198c994d4cadaa76f8cba138c7c31fd169a0
SadLifee <joona.nikkola@gmail.com>
2023-08-02 20:52:18 +0300

    Create Demo1a.java

---
+++ Demo1a.java
@@ -0,0 +1,33 @@
+package Demot;
+
+import java.util.ArrayList;
+
+public class Demo1a {
+    public static void main(String[] args) {
+    }
+}
+
+class Esiintyja {
+    protected String nimi;
+    protected String erikoisala;
+    }
+}
+
+class Asiakas {
+    protected String nimi;
+    protected String kayttajaTunnus;
+    protected String salasana;
+    }
+}
+
+class Tilaisuus {
+    protected String osoite;
+    protected String tyyppi;
+    protected double palkkio;
+    protected boolean onEsiintyja;
+    }
+}
+
+class EsiintyjatTilaisuuksiin {
+    }
+}
